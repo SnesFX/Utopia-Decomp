75ba32229cbb57d057557221b871d898f9c31c2f
Blitz <117093191+SnesFX@users.noreply.github.com>
2023-12-18 11:22:15 -0500

    IT GOES INGAME

---
+++ Assets/Switch/AToStart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AToStart : MonoBehaviour {
+
+    public KeyCode reginput;
+    public KeyCode arrowinput;
+    
+    void Start ()
+    {
+        // nun
+    }
+
+    void Update () {
+
+        if(Input.GetKey(KeyCode.Joystick1Button0))
+        {
+            SceneManager.LoadScene("Sandbox");
+            // Sonic Utopia Para Nintendo Switch Android NSP
+        }      
+    }
+
+}
