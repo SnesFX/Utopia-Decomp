8b8940d1c3a941705909c31f81fc1e802aeb7431
Blitz <117093191+SnesFX@users.noreply.github.com>
2023-12-25 08:42:54 -0500

    optimization stuff + fps shower

---
+++ Assets/Switch/FPS.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour {
+
+    public Text FPSText;
+
+    public float RefreshRate = 1f;
+
+    private float Timer;
+
+    void Update()
+    {
+        if (Time.fixedUnscaledTime > Timer)
+        {
+            int fps = (int)(1f / Time.unscaledDeltaTime);
+            FPSText.text = "FPS: " + fps;
+            Timer = Time.unscaledTime + RefreshRate;
+        }
+    }
+}
